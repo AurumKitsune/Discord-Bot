814bf35958ff880598d7ffc2d648bea9174056d7
Aurum <AurumKitsune@gmail.com>
2021-11-08 14:29:55 -0600

    work with multiple guild ids

--- delete-commands.js
+++ delete-commands.js
@@ -1,6 +1,6 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { guildIds, clientId, token } = require('./config.json');
 
 const rest = new REST({ version: '9' }).setToken(token);
 
@@ -13,10 +13,12 @@ const rest = new REST({ version: '9' }).setToken(token);
 			{ body: [] }
 		);
 
-		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: [] }
-		);
+		for (let server in guildIds) {
+			await rest.put(
+				Routes.applicationGuildCommands(clientId, guildIds[server]),
+				{ body: [] }
+			);
+		}
 
 		console.log('Successfully deleted application (/) commands.');
 	} catch (error) {
