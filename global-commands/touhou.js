005d1b3dc69a16fe21ab00669a8c16070a1c87e6
Aurum <AurumKitsune@gmail.com>
2021-11-08 15:51:56 -0600

    add touhou command

---
+++ global-commands/touhou.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+let fs = require('fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('touhou')
+		.setDescription('Send a random touhou picture/gif/video'),
+	async execute(interaction) {
+		fs.readFile('res/touhouLinks.txt', 'utf8', function(err, data) {
+			if (err) throw err;
+			data = data.split('\n');
+
+			interaction.reply(data[Math.floor(Math.random() * data.length)]);
+		});
+	}
+};
