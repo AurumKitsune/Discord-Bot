53b8fe4caa9d9c769610f7958c66991fc4a75a4f
Aurum <AurumKitsune@gmail.com>
2021-11-08 14:28:14 -0600

    add reddit channel reactions

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,8 +3,13 @@ module.exports = {
 	execute(message){
 		if (message.author.bot) return;
 
-		if (message.content.match(/.*miku time.*/gi)) {
+		if (message.content.match(/.*miku time.*/i)) {
 			message.channel.send('Miku Time!');
+		}
+
+		if (message.channel.name.match(/reddit/i)) {
+			message.react('907363962700595201');
+			message.react('907363946678337596');
 		}
 	}
 }
