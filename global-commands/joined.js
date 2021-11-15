9b38a2c89dacf84b02543d0a1f1d1e021f88df33
Aurum <AurumKitsune@gmail.com>
2021-11-15 16:34:22 -0600

    add multiple page support

--- global-commands/joined.js
+++ global-commands/joined.js
@@ -4,7 +4,11 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('joined')
-		.setDescription('Replies with user info'),
+		.setDescription('Replies with user info')
+		.addIntegerOption(option => 
+			option.setName('page')
+				.setDescription('Page of leaderboard you want to see')
+		),
 	async execute(interaction) {
 		await interaction.deferReply();
 
@@ -13,17 +17,33 @@ module.exports = {
 			.filter(member => member.user.bot == false)
 			.sort((curr, prev) => curr.joinedTimestamp > prev.joinedTimestamp ? 1 : -1);
 
+		const pages = Math.ceil(memberList.size / 10);
+
+		let pageNum = 1;
+		if (interaction.options.getInteger('page')) {
+			pageNum = interaction.options.getInteger('page');
+		}
+		
+		if (pageNum <= 0 || pageNum > pages) {
+			let errorStr = 'Please select a valid page number';
+			if (pages > 1) {
+				errorStr += ' (1-' + pages + ')';
+			}
+
+			await interaction.editReply(errorStr);
+			return;
+		}
+
 		let str = '';
-		for (let i = 0; i < memberList.size && i < 10; i++) {
+		for (let i = (pageNum - 1) * 10; i < memberList.size && i < 10; i++) {
 			str += `${i+1}: ${memberList.at(i).displayName} - <t:${Math.floor(memberList.at(i).joinedTimestamp / 1000)}:D> \n`;
 		}
 		
 		const joinedEmbed = new MessageEmbed()
 			.setColor('#86CECB')
 			.setTitle('Joined Leaderboard')
-			.addFields(
-				{name: '\u200B', value: str, inline: true}
-			);
+			.addField('\u200B', str, false)
+			.setFooter('page: ' + pageNum + '/' + pages);
 		await interaction.editReply({embeds: [joinedEmbed]});
 	}
 };
