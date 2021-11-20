59abde03229af7671ebcb961b2eb1d54fb8dab09
Aurum <AurumKitsune@gmail.com>
2021-11-20 14:11:45 -0600

    add and create replit database

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const token = process.env['token'];
 
+const Database = require("@replit/database");
+const db = new Database();
+console.log(process.env['REPLIT_DB_URL']);
+
 // Declare bot Intents
 const myIntents = new Intents();
 myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS);
