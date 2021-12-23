630c9fcd56c700618d24e9eba3ab79212f5d7d38
Aurum <AurumKitsune@gmail.com>
2021-12-22 18:31:03 -0600

    add code to fetch message if partial

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,11 +3,21 @@ const db = new Database();
 
 module.exports = {
 	name: 'messageReactionAdd',
-	async execute(messageReaction, user){
+	async execute(reaction, user){
+		if (reaction.partial) {
+			try {
+				await reaction.fetch();
+			}
+			catch(error) {
+				console.error('Something went wrong when fetching the message:', error);
+				return;
+			}
+		}
+
 		if (user.bot) return;
 
-		if (messageReaction.message.channel.name.match(/reddit/i)) {
-			if (messageReaction.emoji.id == '907363962700595201' || messageReaction.emoji.id == '907363946678337596') {
+		if (reaction.message.channel.name.match(/reddit/i)) {
+			if (reaction.emoji.id == '907363962700595201' || reaction.emoji.id == '907363946678337596') {
 				let userData = {};
 				if (await db.get(user.id)) {
 					userData = await db.get(user.id);
@@ -16,10 +26,10 @@ module.exports = {
 					userData.karma = 0;
 				}
 				
-				if (messageReaction.emoji.id == '907363962700595201') {
+				if (reaction.emoji.id == '907363962700595201') {
 					userData.karma++;
 				}
-				else if (messageReaction.emoji.id == '907363946678337596') {
+				else if (reaction.emoji.id == '907363946678337596') {
 					userData.karma--;
 				}
 				
