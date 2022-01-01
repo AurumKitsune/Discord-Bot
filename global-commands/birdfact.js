804f2af79d22a85371bddd0272637d534cbc73e9
Aurum <AurumKitsune@gmail.com>
2022-01-01 07:11:43 -0600

    make birdfact command better

--- global-commands/birdfact.js
+++ global-commands/birdfact.js
@@ -9,7 +9,7 @@ module.exports = {
 		fs.readFile('res/birdFacts.txt', 'utf8', function(err, data) {
 			if (err) throw err;
 			data = data.split('\n');
-			interaction.reply(data[Math.floor(Math.random() * data.length)]);
+			interaction.reply(`${data[Math.floor(Math.random() * data.length)]}\nBIRD FACT`);
 		});
 	}
 };
